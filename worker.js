a0964610460da6ba9e924197f50dba198520c3bc
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2026-03-05 16:38:55 +1300

    v0.3
    1. Fix that solver changes URL.
    2. Remove dependency on CSV files.

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-importScripts("https://unpkg.com/javascript-lp-solver/prod/solver.js");
+importScripts("https://unpkg.com/javascript-lp-solver/dist/solver.global.js");
 onmessage = function(event) {
   const model = event.data;
   const result = solver.Solve(model);
